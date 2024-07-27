c44c67727ff0ab040589a9dc9551e65f38f07def
harold mei <haroldmei.cn@gmail.com>
2024-07-27 14:29:47 +1000

    release the training output

--- src/select_model.py
+++ src/select_model.py
@@ -2,17 +2,26 @@ import argparse
 from glob import glob
 import pandas as pd
 import json
+import os
 
 def main(args):
     files = glob(f'{args.basedir}/*/{args.file}')
     items = []
     for f in files:
         with open(f, 'r') as f:
-            items.append(json.loads(f.read()))
+            dd = json.loads(f.read())
+            dd['folder'] = f
+            items.append(dd)
     df = pd.DataFrame.from_records(items)
     idx = df[args.col].argmax()
     with open(f'{args.basedir}/selected_{args.file}', 'w') as f:
         f.write(df.iloc[idx].to_json())
+
+    # remove unselected results
+    for f in files:
+        if f != df['folder'].tolist()[idx]:
+            os.rmdir(f[:-12]) 
+            print('removing unselected result: ', f[:-12])
 
 if __name__ == '__main__':
     
